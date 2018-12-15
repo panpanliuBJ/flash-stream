5e9d2c5629469877662db363417cde9911e4bd49
liupanpan <panpan.liu@bitmain.com>
2018-12-15 10:59:49 +0800

    convert to web application for export metric of prometheus

---
+++ src/test/java/com/finley/flash/stream/StreamApplicationTest.java
@@ -0,0 +1,19 @@
+package com.finley.flash.stream;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StreamApplicationTest {
+
+    @Test
+    public void name() {
+
+    }
+}
