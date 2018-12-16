49bb5a122da67c51b283e412741ff609e8a8e6c6
liupanpan <panpan.liu@bitmain.com>
2018-12-16 10:07:16 +0800

    replace deprecated method of kafka stream

--- src/main/java/com/finley/flash/stream/kafka/topology/UserSubTopology.java
+++ src/main/java/com/finley/flash/stream/kafka/topology/UserSubTopology.java
@@ -2,14 +2,15 @@ package com.finley.flash.stream.kafka.topology;
 
 import com.finley.flash.stream.kafka.model.MiningData;
 import com.finley.flash.stream.kafka.model.MiningTopologyMeta;
+import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Serialized;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.apache.kafka.streams.kstream.Windowed;
 import org.apache.kafka.streams.state.WindowStore;
@@ -32,12 +33,12 @@ public class UserSubTopology extends AbstractTopology<MiningData> implements
 
     @Override
     public KStream<Windowed<String>, MiningData> doAggregate(KStream<String, MiningData> source, String storeName, String toTopic) {
-        long windowSizeMs = getWindowSizeMs();
-        //已经是groupBy之后的结果
+        final Duration timeWindowSize = Duration.ofSeconds(meta.getTimeUnit().toSeconds(meta.getTimeValue()));
+
         KStream<Windowed<String>, MiningData> summaryKStream = source.filter((k, v) -> v != null)
             .map((k, v) -> new KeyValue<>(v.getKey(), v))
-            .groupBy((k, v) -> k, Serialized.with(Serdes.String(), StreamUtil.jsonSerde(MiningData.class)))
-            .windowedBy(TimeWindows.of(windowSizeMs).advanceBy(windowSizeMs))
+            .groupBy((k, v) -> k, Grouped.with(Serdes.String(), StreamUtil.jsonSerde(MiningData.class)))
+            .windowedBy(TimeWindows.of(timeWindowSize).advanceBy(timeWindowSize))
             .aggregate(() -> new MiningData(),
                 (aggKey, newValue, aggValue) -> aggValue.add(newValue, toMillis(meta.getTimeUnit(), meta.getTimeValue())),
                 Materialized.<String, MiningData, WindowStore<Bytes, byte[]>>as(storeName).withValueSerde(StreamUtil.jsonSerde(MiningData.class)))
